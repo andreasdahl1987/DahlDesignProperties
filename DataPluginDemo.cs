da4f5686d8865e96247dbd97a65629e0e158035f
andreasdahl1987 <40788634+andreasdahl1987@users.noreply.github.com>
2022-11-08 00:09:02 +0100

    Changed version number

--- DataPluginDemo.cs
+++ DataPluginDemo.cs
@@ -6372,7 +6372,7 @@ namespace User.PluginSdkDemo
 
             //Update property
 
-            pluginManager.AddProperty("Version", this.GetType(), "1.10.0");
+            pluginManager.AddProperty("Version", this.GetType(), "1.11.0");
 
             //Key presses
             pluginManager.AddProperty("FuelSaveDelta", this.GetType(), 0);
