d51d6a133efe61b87c599dd825ac9480a70ea763
photon1503 <g.hitz@gmx.at>
2022-11-22 16:19:38 +0100

    added actions for every single screen

--- Sections/Dashboard.cs
+++ Sections/Dashboard.cs
@@ -53,8 +53,29 @@ namespace DahlDesign.Plugin.Categories
             Base.AddAction("Controls.Dashboard.LeftScreen.Next", (a, b) => LeftScreen.Next());
             Base.AddAction("Controls.Dashboard.LeftScreen.Previous", (a, b) => LeftScreen.Previous());
 
+            //"TimeAttack", "Sector", "LapTime", "Quali", "Race", "Race2", "Race3", "ShiftPoint", "Map"
+            Base.AddAction("Controls.Dashboard.LeftScreen.1_TimeAttack", (a, b) => LeftScreen.screenID=1);
+            Base.AddAction("Controls.Dashboard.LeftScreen.2_Sector", (a, b) => LeftScreen.screenID=2);
+            Base.AddAction("Controls.Dashboard.LeftScreen.3_LapTime", (a, b) => LeftScreen.screenID=3);
+            Base.AddAction("Controls.Dashboard.LeftScreen.4_Quali", (a, b) => LeftScreen.screenID=4);
+            Base.AddAction("Controls.Dashboard.LeftScreen.5_Race", (a, b) => LeftScreen.screenID=5);
+            Base.AddAction("Controls.Dashboard.LeftScreen.6_Race2", (a, b) => LeftScreen.screenID=6);
+            Base.AddAction("Controls.Dashboard.LeftScreen.7_Race3", (a, b) => LeftScreen.screenID=7);
+            Base.AddAction("Controls.Dashboard.LeftScreen.8_ShiftPoint", (a, b) => LeftScreen.screenID=8);
+            Base.AddAction("Controls.Dashboard.LeftScreen.9_Map", (a, b) => LeftScreen.screenID=9);
+
             Base.AddAction("Controls.Dashboard.RightScreen.Next", (a, b) => RightScreen.Next());
             Base.AddAction("Controls.Dashboard.RightScreen.Previous", (a, b) => RightScreen.Previous());
+
+            //"Practice", "Stint", "Q1", "Q2", "Race1", "Race2", "Ralley", "Track"
+            Base.AddAction("Controls.Dashboard.RightScreen.1_Practice", (a, b) => RightScreen.screenID=1);
+            Base.AddAction("Controls.Dashboard.RightScreen.2_Stint", (a, b) => RightScreen.screenID=2);
+            Base.AddAction("Controls.Dashboard.RightScreen.3_Q1", (a, b) => RightScreen.screenID=3);
+            Base.AddAction("Controls.Dashboard.RightScreen.4_Q2", (a, b) => RightScreen.screenID=4);
+            Base.AddAction("Controls.Dashboard.RightScreen.5_Race1", (a, b) => RightScreen.screenID=5);
+            Base.AddAction("Controls.Dashboard.RightScreen.6_Race2", (a, b) => RightScreen.screenID=6);
+            Base.AddAction("Controls.Dashboard.RightScreen.7_Ralley", (a, b) => RightScreen.screenID=7);
+            Base.AddAction("Controls.Dashboard.RightScreen.8_Track", (a, b) => RightScreen.screenID=8);
         }
 
         public void DataUpdate()
